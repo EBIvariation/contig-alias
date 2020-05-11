60fdf6f7691e1fccec1820c6a94da708fde86bb6
Tanmay Singal <tanmaysingal@icloud.com>
2020-05-11 15:21:50 +0530

    Created new Spring project

---
+++ src/test/java/com/ebivariation/contigalias/ContigAliasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebivariation.contigalias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContigAliasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
