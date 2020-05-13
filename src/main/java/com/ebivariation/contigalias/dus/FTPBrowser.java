40ed42e4809e9e2f24c5be9402dc638c12eaa427
Tanmay Singal <tanmaysingal@icloud.com>
2020-05-13 19:26:24 +0530

    Methods throw exceptions instead of handling themselves

--- src/main/java/com/ebivariation/contigalias/dus/FTPBrowser.java
+++ src/main/java/com/ebivariation/contigalias/dus/FTPBrowser.java
@@ -8,74 +8,60 @@ import java.io.IOException;
 
 public class FTPBrowser {
 
-    private FTPClient ftp = null;
+    private FTPClient ftp = new FTPClient();
 
-    public void connect() {
+    public void connect() throws IOException {
 
-        ftp = new FTPClient();
-        try {
-            int reply;
-            String server = "ftp.ncbi.nlm.nih.gov";
+        int reply;
+        String server = "ftp.ncbi.nlm.nih.gov";
 
-            ftp.connect(server, 21);
-            System.out.println("Connected to " + server + ".");
-            System.out.print(ftp.getReplyString());
+        ftp.connect(server, 21);
 
-            // Login as anonymous user
-            boolean login = ftp.login("anonymous", "fakeemail@example.com");
-            System.out.println("Login " + (login ? "successful" : "unsuccessful"));
+        System.out.println("Connected to " + server + ".");
 
-            String status = ftp.getStatus();
-            System.out.println(status);
+        // Login as anonymous user
+        boolean login = ftp.login("anonymous", "jmmut@ebi.ac.uk");
+        System.out.println("Login " + (login ? "successful" : "unsuccessful"));
 
-            // After connection attempt, you should check the reply code to verify
-            // success.
-            reply = ftp.getReplyCode();
+        System.out.println("Directory count:\t" + ftp.listDirectories().length);
 
-            if (!FTPReply.isPositiveCompletion(reply)) {
-                ftp.disconnect();
-                System.err.println("FTP server refused connection.");
-            }
+        String status = ftp.getStatus();
+        System.out.println(status);
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        // After connection attempt, you should check the reply code to verify
+        // success.
+        reply = ftp.getReplyCode();
+
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            System.err.println("FTP server refused connection.");
         }
-
     }
 
-    public void disconnect() {
+    public void disconnect() throws IOException {
         if (ftp != null) {
             if (ftp.isConnected()) {
-                try {
-                    ftp.logout();
-                    ftp.disconnect();
-                    ftp = null;
-                } catch (IOException ioe) {
-                    // do nothing
-                }
+                ftp.logout();
+                ftp.disconnect();
+                ftp = null;
             }
         }
     }
 
-    public void listDir() {
-        if (ftp == null) {
-            return;
-        }
+    public FTPFile[] listDir() throws IOException {
 
         FTPFile[] ftpFiles = null;
-        try {
-            ftpFiles = ftp.listFiles();
-        } catch (IOException e) {
-            // Do something --_(-_-)_--
-        }
+        ftpFiles = ftp.listFiles();
 
-        if (ftpFiles != null) {
-            System.out.println(ftpFiles.length);
+//        if (ftpFiles != null) {
+//            System.out.println(ftpFiles.length);
+//
+//            for (FTPFile ftpFile : ftpFiles) {
+//                System.out.println(ftpFile.getRawListing());
+//            }
+//        }
 
-            for (FTPFile ftpFile : ftpFiles) {
-                System.out.println(ftpFile.getRawListing());
-            }
-        }
+        return ftpFiles;
 
     }
 
--- src/test/java/com/ebivariation/contigalias/FTPBrowserTest.java
+++ src/test/java/com/ebivariation/contigalias/FTPBrowserTest.java
@@ -3,14 +3,35 @@ package com.ebivariation.contigalias;
 import com.ebivariation.contigalias.dus.FTPBrowser;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 public class FTPBrowserTest {
 
     @Test
-    void connectToServer(){
+    void connectToServer() {
         FTPBrowser ftpBrowser = new FTPBrowser();
-        ftpBrowser.connect();
-        ftpBrowser.listDir();
-        ftpBrowser.disconnect();
+        try {
+            ftpBrowser.connect();
+            System.out.println(ftpBrowser.listDir().length);
+            ftpBrowser.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void jshellTest(){
+        org.apache.commons.net.ftp.FTPClient ftp = new org.apache.commons.net.ftp.FTPClient();
+        String server = "ftp.ncbi.nlm.nih.gov";
+        try {
+            ftp.connect(server, 21);
+
+        boolean login = ftp.login("anonymous", "jmmut@ebi.ac.uk(opens in new tab)");
+        System.out.println(login);
+        System.out.println(ftp.listDirectories().length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
