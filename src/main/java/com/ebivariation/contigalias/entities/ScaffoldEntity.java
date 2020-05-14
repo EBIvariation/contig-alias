694a80b15e6ae2aae2d1fecf6147e805aa32b076
Tanmay Singal <tanmaysingal@icloud.com>
2020-05-14 12:46:13 +0530

    Added POJOs for Assemblies and Scaffolds

---
+++ src/main/java/com/ebivariation/contigalias/entities/ScaffoldEntity.java
@@ -0,0 +1,12 @@
+package com.ebivariation.contigalias.entities;
+
+public class ScaffoldEntity {
+
+    private long id;
+    private String name;
+    private String genbank;
+    private String refseq;
+
+    private AssemblyEntity assembly;
+
+}
