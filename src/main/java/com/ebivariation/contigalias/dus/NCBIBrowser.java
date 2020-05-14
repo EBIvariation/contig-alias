dcc4fb2042cf4521b06d253a873323db6cfbc5a9
Tanmay Singal <tanmaysingal@icloud.com>
2020-05-14 15:01:27 +0530

    Added documentation related to getGenomeReportDirectory()

--- src/main/java/com/ebivariation/contigalias/dus/NCBIBrowser.java
+++ src/main/java/com/ebivariation/contigalias/dus/NCBIBrowser.java
@@ -23,23 +23,36 @@ public class NCBIBrowser extends FTPBrowser {
         navigateToSubDirectoryPath(PATH_GENOMES_ALL);
     }
 
-    public String getGenomeReportDirectory(String query) throws IOException {
+    /**
+     * Takes a Genbank or Refseq accession and converts it to the equivalent path used by NCBI's FTP server.
+     * For example, on input "GCF_007608995.1" the output path is "GCF/007/608/995/GCF_007608995.1_ASM760899v1/".
+     * @param accession Any GCA or GCF String
+     * @return Path relative to ftp.ncbi.nlm.nih.gov/genomes/all/
+     * @throws IOException Passes exception thrown by FTPBrowser.listDirectories()
+     */
+    public String getGenomeReportDirectory(String accession) throws IOException {
 
         //GCA_004051055.1
-        String rawQuery = query;
-        //GCA/004/051/055/
+        String rawQuery = accession;
         String path = "";
 
-        path += query.substring(0, 3) + "/";
-        query = query.substring(4);
+        // path = "GCA/"
+        path += accession.substring(0, 3) + "/";
+        // accession = "004051055.1"
+        accession = accession.substring(4);
 
-        path += query.substring(0, 3) + "/";
-        query = query.substring(3);
+        // path = "GCA/004/"
+        path += accession.substring(0, 3) + "/";
+        // accession = "051055.1"
+        accession = accession.substring(3);
 
-        path += query.substring(0, 3) + "/";
-        query = query.substring(3);
+        // path = "GCA/004/051/"
+        path += accession.substring(0, 3) + "/";
+        // accession = "055.1"
+        accession = accession.substring(3);
 
-        path += query.substring(0, 3) + "/";
+        // path = "GCA/004/051/055/"
+        path += accession.substring(0, 3) + "/";
 
         String currPath = PATH_GENOMES_ALL + path;
         FTPFile[] ftpFiles = super.listDirectories(currPath);
@@ -47,6 +60,7 @@ public class NCBIBrowser extends FTPBrowser {
         if (ftpFiles.length > 0) {
             String dirName = ftpFiles[0].getName();
             if (dirName.contains(rawQuery)) {
+                // path = "GCA/004/051/055/GCA_004051055.1_ASM405105v1/"
                 path += dirName + "/";
             }
         } else path = null;
