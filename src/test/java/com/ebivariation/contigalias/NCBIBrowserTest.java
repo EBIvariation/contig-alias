ec823c884405b95dee045af3f032c2aa8c1fdbd4
Tanmay Singal <tanmaysingal@icloud.com>
2020-05-15 17:05:46 +0530

    Removed redundant code by adding setup and teardown methods

--- src/test/java/com/ebivariation/contigalias/NCBIBrowserTest.java
+++ src/test/java/com/ebivariation/contigalias/NCBIBrowserTest.java
@@ -1,6 +1,8 @@
 package com.ebivariation.contigalias;
 
 import com.ebivariation.contigalias.dus.NCBIBrowser;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -10,62 +12,46 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NCBIBrowserTest {
 
+    private NCBIBrowser ncbiBrowser;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        ncbiBrowser = new NCBIBrowser();
+        ncbiBrowser.connect();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        ncbiBrowser.disconnect();
+    }
+
     @Test
     void connect() throws IOException {
-        NCBIBrowser ncbiBrowser = new NCBIBrowser();
-        try {
-            ncbiBrowser.connect();
-        } finally {
-            ncbiBrowser.disconnect();
-        }
+        ncbiBrowser.connect();
     }
 
     @Test
     void navigateToAllGenomesDirectory() throws IOException {
-        NCBIBrowser ncbiBrowser = new NCBIBrowser();
-        try {
-            ncbiBrowser.connect();
             ncbiBrowser.navigateToAllGenomesDirectory();
             assertTrue(ncbiBrowser.listFiles().length > 0);
-        } finally {
-            ncbiBrowser.disconnect();
-        }
     }
 
     @Test
     void navigateToSubDirectoryPath() throws IOException {
-        NCBIBrowser ncbiBrowser = new NCBIBrowser();
-        try {
-            ncbiBrowser.connect();
             ncbiBrowser.navigateToSubDirectoryPath("/genomes/INFLUENZA/");
             assertTrue(ncbiBrowser.listFiles().length > 0);
-        } finally {
-            ncbiBrowser.disconnect();
-        }
     }
 
     @Test
-    void getGenomeReportDirectory_GCATest() throws IOException {
-        NCBIBrowser ncbiBrowser = new NCBIBrowser();
-        try {
-            ncbiBrowser.connect();
+    void getGenomeReportDirectoryGCATest() throws IOException {
             String path = ncbiBrowser.getGenomeReportDirectory("GCA_004051055.1");
             assertEquals("GCA/004/051/055/GCA_004051055.1_ASM405105v1/", path);
-        } finally {
-            ncbiBrowser.disconnect();
-        }
     }
 
     @Test
-    void getGenomeReportDirectory_GCFTest() throws IOException {
-        NCBIBrowser ncbiBrowser = new NCBIBrowser();
-        try {
-            ncbiBrowser.connect();
+    void getGenomeReportDirectoryGCFTest() throws IOException {
             String path = ncbiBrowser.getGenomeReportDirectory("GCF_007608995.1");
             assertEquals("GCF/007/608/995/GCF_007608995.1_ASM760899v1/", path);
-        } finally {
-            ncbiBrowser.disconnect();
-        }
     }
 
 }
