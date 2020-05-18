c034ff36c783bf9e09be4e7d778c1755b49526b0
Tanmay Singal <tanmaysingal@icloud.com>
2020-05-18 14:27:08 +0530

    Added a local testing resource for AssemblyReportReaderTest

--- src/test/java/com/ebivariation/contigalias/dus/AssemblyReportReaderTest.java
+++ src/test/java/com/ebivariation/contigalias/dus/AssemblyReportReaderTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,16 +17,12 @@ class AssemblyReportReaderTest {
 
     InputStreamReader streamReader;
 
-    private NCBIBrowser ncbiBrowser;
-
     private InputStream stream;
 
     @BeforeEach
-    void setUp() throws IOException {
-        ncbiBrowser = new NCBIBrowser();
-        ncbiBrowser.connect();
-        stream = ncbiBrowser.getAssemblyReportInputStream(
-                "/genomes/all/GCF/007/608/995/GCF_007608995.1_ASM760899v1/");
+    void setup() throws FileNotFoundException {
+        stream = new FileInputStream(
+                new File("src/test/resources/GCF_000003055.4_Bos_taurus_UMD_3.1_assembly_report.txt"));
         streamReader = new InputStreamReader(stream);
     }
 
@@ -31,11 +30,10 @@ class AssemblyReportReaderTest {
     void tearDown() throws IOException {
         stream.close();
         streamReader.close();
-        ncbiBrowser.disconnect();
     }
 
     @Test
-    void getAssemblyReportReader() throws IOException {
+    void getAssemblyReportReader() {
         AssemblyReportReader reader = new AssemblyReportReader(streamReader);
         assertTrue(reader.lines().count() > 0);
     }
