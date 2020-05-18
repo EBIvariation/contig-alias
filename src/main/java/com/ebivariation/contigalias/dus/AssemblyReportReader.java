80625d0e1214ae8f82eb12bb057978c7af09405f
Tanmay Singal <tanmaysingal@icloud.com>
2020-05-18 14:54:07 +0530

    Implemented parsing of AssemblyData

--- src/main/java/com/ebivariation/contigalias/dus/AssemblyReportReader.java
+++ src/main/java/com/ebivariation/contigalias/dus/AssemblyReportReader.java
@@ -1,11 +1,15 @@
 package com.ebivariation.contigalias.dus;
 
+import com.ebivariation.contigalias.entities.AssemblyEntity;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class AssemblyReportReader extends BufferedReader {
+
+    private final AssemblyEntity assemblyEntity = new AssemblyEntity();
 
     public AssemblyReportReader(Reader reader) {
         super(reader);
@@ -22,7 +26,7 @@ public class AssemblyReportReader extends BufferedReader {
             return null;
         }
 
-        if (line.startsWith("#")) {
+        if (line.startsWith("# ")) {
             parseAssemblyData(line);
         } else {
             parseScaffoldLine(line);
@@ -32,7 +36,32 @@ public class AssemblyReportReader extends BufferedReader {
     }
 
     private void parseAssemblyData(String line) {
-        //TODO
+        int tagEnd = line.indexOf(':');
+        if (tagEnd == -1) return;
+        String tag = line.substring(2, tagEnd);
+        String tagData = line.substring(tagEnd + 1).trim();
+        switch (tag) {
+            case "Assembly name": {
+                assemblyEntity.setName(tagData);
+                break;
+            }
+            case "Organism name": {
+                assemblyEntity.setOrganism(tagData);
+                break;
+            }
+            case "Taxid": {
+                assemblyEntity.setTaxid(Long.parseLong(tagData));
+                break;
+            }
+            case "GenBank assembly accession": {
+                assemblyEntity.setGenbank(tagData);
+                break;
+            }
+            case "RefSeq assembly accession": {
+                assemblyEntity.setRefseq(tagData);
+                break;
+            }
+        }
     }
 
     private void parseScaffoldLine(String line) {
