a9d18d2091c7a19b9f356685e7519cda94080239
singaltanmay <tanmaysingal@icloud.com>
2020-05-26 23:41:33 +0530

    Add @Repository notation to Assembly & Chromosome repositories

--- src/main/java/com/ebivariation/contigalias/repo/AssemblyRepository.java
+++ src/main/java/com/ebivariation/contigalias/repo/AssemblyRepository.java
@@ -18,9 +18,11 @@ package com.ebivariation.contigalias.repo;
 
 import com.ebivariation.contigalias.entities.AssemblyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AssemblyRepository extends JpaRepository<AssemblyEntity, Long> {
 
     default Optional<AssemblyEntity> findAssemblyEntityByAccession(String accession) {
--- src/main/java/com/ebivariation/contigalias/repo/ChromosomeRepository.java
+++ src/main/java/com/ebivariation/contigalias/repo/ChromosomeRepository.java
@@ -18,7 +18,9 @@ package com.ebivariation.contigalias.repo;
 
 import com.ebivariation.contigalias.entities.ChromosomeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChromosomeRepository extends JpaRepository<ChromosomeEntity, Long> {
 
 }
